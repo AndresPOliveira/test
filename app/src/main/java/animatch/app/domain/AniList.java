f822568c87c0eaa94e383056a70f8754178d185f
andresPOliveira <andres.oliveira@sptech.school>
2023-10-07 22:13:17 -0300

    remove set list

--- app/src/main/java/animatch/app/domain/AniList.java
+++ app/src/main/java/animatch/app/domain/AniList.java
@@ -8,21 +8,14 @@ import jakarta.persistence.Table;
 import java.util.Set;
 
 @Entity
-@Table(name = "ANILIST")
 public class AniList {
 
     @Id
     private int id ;
-    @OneToMany(mappedBy = "anilist")
-    private Set<List> lists;
     private int animeId;
 
     public int getId() {
         return id;
-    }
-
-    public Set<List> getLists() {
-        return lists;
     }
 
     public int getAnimeId() {
